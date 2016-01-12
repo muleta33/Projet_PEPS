e5f4cfe2c2f2175758b43f70748bdea93606638b
margotj38 <john-elie.margot@ensimag.grenoble-inp.fr>
2016-01-12 18:28:02 +0100

    Temporisation scripts

--- profitAndLossTests/launcherCall300000.bat
+++ profitAndLossTests/launcherCall300000.bat
@@ -3,20 +3,24 @@
 
 echo --- 1 per month --- >> call300000.txt
 for /l %%A in (1 1 20) do (
+timeout /t 2 /nobreak > NUL
 start /wait cmd /c "..\x64\Debug\profitAndLoss.exe ..\dat\Call_month_300000.dat >> call300000.txt
 )
 
 echo --- 1 per week --- >> call300000.txt
 for /l %%A in (1 1 20) do (
+timeout /t 2 /nobreak > NUL
 start /wait cmd /c "..\x64\Debug\profitAndLoss.exe ..\dat\Call_week_300000.dat >> call300000.txt
 )
 
 echo --- 1 per day --- >> call300000.txt
 for /l %%A in (1 1 20) do (
+timeout /t 2 /nobreak > NUL
 start /wait cmd /c "..\x64\Debug\profitAndLoss.exe ..\dat\Call_day_300000.dat >> call300000.txt
 )
 
 echo --- 5 per day --- >> call300000.txt
 for /l %%A in (1 1 20) do (
+timeout /t 2 /nobreak > NUL
 start /wait cmd /c "..\x64\Debug\profitAndLoss.exe ..\dat\Call_5_per_day_300000.dat >> call300000.txt
 )

541740c17e4b17f3f760d4f757d4af19edcb29b0
margotj38 <john-elie.margot@ensimag.grenoble-inp.fr>
2016-01-12 18:21:21 +0100

    Mise à jour scripts

--- profitAndLossTests/launcherCall300000.bat
+++ profitAndLossTests/launcherCall300000.bat
@@ -2,21 +2,21 @@
 @echo off
 
 echo --- 1 per month --- >> call300000.txt
-for /l %%A in (1 1 10) do (
+for /l %%A in (1 1 20) do (
 start /wait cmd /c "..\x64\Debug\profitAndLoss.exe ..\dat\Call_month_300000.dat >> call300000.txt
 )
 
 echo --- 1 per week --- >> call300000.txt
-for /l %%A in (1 1 10) do (
+for /l %%A in (1 1 20) do (
 start /wait cmd /c "..\x64\Debug\profitAndLoss.exe ..\dat\Call_week_300000.dat >> call300000.txt
 )
 
 echo --- 1 per day --- >> call300000.txt
-for /l %%A in (1 1 10) do (
+for /l %%A in (1 1 20) do (
 start /wait cmd /c "..\x64\Debug\profitAndLoss.exe ..\dat\Call_day_300000.dat >> call300000.txt
 )
 
 echo --- 5 per day --- >> call300000.txt
-for /l %%A in (1 1 10) do (
+for /l %%A in (1 1 20) do (
 start /wait cmd /c "..\x64\Debug\profitAndLoss.exe ..\dat\Call_5_per_day_300000.dat >> call300000.txt
 )

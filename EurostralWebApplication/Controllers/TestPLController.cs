6551e9953243593f18bd32c3b822d9fcda32ed77
muleta33 <antoine.mulet@ensimag.grenoble-inp.fr>
2016-03-26 20:09:53 +0100

    page Validation fonctionnelle

---
+++ EurostralWebApplication/Controllers/TestPLController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DotNet.Highstock.Enums;
+using DotNet.Highstock.Helpers;
+using DotNet.Highstock.Options;
+using EurostralWebApplication.Models;
+
+namespace EurostralWebApplication.Controllers
+{
+    public class TestPLController : Controller
+    {
+        //
+        // GET: /TestPL/
+        public ActionResult Index()
+        {
+            ViewModelTestPL model = new ViewModelTestPL();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult onChangeTest(ViewModelTestPL model)
+        {
+            List<object[]> val = model.getCurrentMode().getRandomPLData();
+
+            List<object[]> serie1 = new List<object[]>();
+            List<object[]> serie2 = new List<object[]>();
+
+            foreach (var obj in val)
+            {
+                serie1.Add(new object[] { obj[0], obj[1] });
+                serie2.Add(new object[] { obj[0], obj[2] });
+            }
+
+            object[] s1 = serie1.ToArray();
+            object[] s2 = serie2.ToArray();
+
+            DotNet.Highstock.Highstock chart = new DotNet.Highstock.Highstock("chart")
+                .InitChart(new Chart
+                {
+                    Type = ChartTypes.Line,
+                    ClassName = "chart"
+                })
+                .SetRangeSelector(new RangeSelector
+                {
+                    AllButtonsEnabled = true,
+                    Selected = 1
+                })
+                .SetTitle(new Title
+                {
+                    Text = model.getCurrentMode().Name
+                })
+                .SetYAxis(new YAxis
+                {
+                    Title = new YAxisTitle { Text = "Valeur" }
+                })
+                .SetTooltip(new Tooltip
+                {
+                    ValueDecimals = 4
+                })
+                .SetSeries(new[]
+                {
+                    new Series { Name = "Portefeuille de couverture", Data = new Data(s1) },
+                    new Series { Name = "Produit", Data = new Data(s2) }
+                })
+                ;
+
+            return PartialView("PartialViewTestPL", chart);
+        }
+
+	}
+}
---
+++ EurostralWebApplication/Models/ViewModelTestPL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EurostralWebApplication.Models
+{
+    public class ViewModelTestPL
+    {
+
+        private readonly List<TestMode> testModes;
+
+        [Display(Name = "Choix du test : ")]
+        public String selectedTestMode { get; set; }
+
+        public ViewModelTestPL()
+        {
+            testModes = new List<TestMode> { new TestMode("BackTest", "Back Test"), new TestMode("ForwardTest", "Forward Test") };
+            selectedTestMode = "BackTest";
+        }
+
+        public IEnumerable<SelectListItem> modeItems
+        {
+            get { return new SelectList(testModes, "Id", "Name"); }
+        }
+
+        public TestMode getCurrentMode()
+        {
+            return testModes.Find(x => x.Id == selectedTestMode);
+        }
+
+    }
+}
